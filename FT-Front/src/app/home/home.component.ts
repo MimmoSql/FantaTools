3998c7b8abe0e3fe2b96eb3e062b1d6bfe8a9fab
Domenico Paolo <domenicopaolo.plati@gmail.com>
2022-07-07 15:24:30 +0200

    Team Page
    [Home Page]
    -Removed team list
    
    [Team Page]
    -Added Team logo list
    
    [Team card]
    -New team card design

--- FT-Front/src/app/home/home.component.ts
+++ FT-Front/src/app/home/home.component.ts
@@ -1,36 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FtServiceService } from '../ft-service.service';
-import { Team } from '../model/objects/Team';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-  stopTeam = false;
-  searchControllerTeam = new FormControl();
-  searchResultTeam: Team[] = [];
-  searchStatus = true;
-  model: FtServiceService;
-
-  constructor(model:FtServiceService) {
-    this.model=model;
-  }
-
-  ngOnInit(): void {
-    this.searchTeam();
-  }
-
-  searchTeam(){
-    console.log("hello");
-    this.stopTeam = true;
-    this.model.searchAllTeamRest(
-      (status: boolean, response: any)=>{
-        this.searchStatus = status;
-        if(status)
-          this.searchResultTeam = response;
-    });
-  }
-}
+export class HomeComponent{}
--- FT-Front/src/app/team/team.component.ts
+++ FT-Front/src/app/team/team.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FtServiceService } from '../ft-service.service';
+import { Team } from '../model/objects/Team';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
-export class TeamComponent implements OnInit {
+export class TeamComponent  implements OnInit{
+  stopTeam = false;
+  searchControllerTeam = new FormControl();
+  searchResultTeam: Team[] = [];
+  searchStatus = true;
+  model: FtServiceService;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(model:FtServiceService) {
+    this.model=model;
   }
 
+  ngOnInit(): void {
+    this.searchTeam();
+  }
+
+  searchTeam(){
+    console.log("hello");
+    this.stopTeam = true;
+    this.model.searchAllTeamRest(
+      (status: boolean, response: any)=>{
+        this.searchStatus = status;
+        if(status)
+          this.searchResultTeam = response;
+    });
+  }
 }
